a0d47bb711c5fbd343c0e3ab377ee6b4249d7991
Vladyslav Krutyus <kijdron@gmail.com>
2023-05-30 13:41:17 +0300

    deleted everything

--- node-lab-topic2/task2.js
+++
@@ -1,7 +0,0 @@
-function areAnagrams(string1, string2) {
-    return string1.split('').sort().join('') === string2.split('').sort().join('');
-}
-
-console.log(areAnagrams("aboba", "aboba"))
-console.log(areAnagrams('listen', 'silent'));
-console.log(areAnagrams('hello', 'world'));
--- node-lab-topic3/task2.ts
+++
@@ -1,7 +0,0 @@
-function areAnagrams(string1: string, string2: string): boolean {
-    return string1.split('').sort().join('') === string2.split('').sort().join('');
-}
-
-console.log(areAnagrams("aboba", "aboba"))
-console.log(areAnagrams('listen', 'silent'));
-console.log(areAnagrams('hello', 'world'));
